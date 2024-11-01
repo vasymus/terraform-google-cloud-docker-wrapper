e34c6fb1087135bae12887265d006dbe817feaf0
vasymus <vasymuszchd@mail.ru>
2024-11-01 16:35:05 +0200

    finalize example

--- workspace/main.tf
+++ workspace/main.tf
@@ -38,10 +38,6 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     machine_type = var.machine_type
 
-    # temporary
-    disk_size_gb = 10
-    disk_type    = "pd-standard"
-
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
